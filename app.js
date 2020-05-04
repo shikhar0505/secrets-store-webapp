966577455fc6361cbb41789065d78a329d59f643
Shikhar Kumar <shkumar@tripadvisor.com>
2020-05-04 02:23:32 -0400

    adding passport, cookie setting support

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // init setup
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require('express-session');
+
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
@@ -14,6 +22,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(session({
+  secret: process.env.PASSPORT_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // db connection
 
@@ -23,14 +38,43 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 });
 mongoose.set("useCreateIndex", true);
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
   googleId: String,
   secrets: [String]
-};
+});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// passport - Google OAuth2.0 connection
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // endpoints
 
@@ -43,6 +87,56 @@ app.route("/register")
   res.render("register");
 })
 .post(function(req, res){
+  User.register({username:req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.route("/login")
+.get(function(req, res){
+  res.render("login");
+})
+.post(function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("login");
+  }
+});
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+/*
+// bcrypt implementations
+// commenting out in favor of the passportjs implementation above
+
+app.get("/register", function(req, res){
   bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
     if(err) {
       console.log(err);
@@ -57,13 +151,9 @@ app.route("/register")
       res.render("secrets-bcrypt");
     }
   });
-});
+}
 
-app.route("/login")
-.get(function(req, res){
-  res.render("login");
-})
-.post(function(req, res){
+app.get("/login", function(req, res){
   User.findOne({email: req.body.username}, function(err, foundUser) {
     if (err) {
       console.log(err);
@@ -80,11 +170,8 @@ app.route("/login")
       }
     }
   });
-});
-
-// app.get("/secrets", function(req, res){
-//   res.render("secrets");
-// });
+}
+*/
 
 // listener
 
