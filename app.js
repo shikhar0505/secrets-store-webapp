36e3fb00eb54dfefc60c28c7b11e10076b17a24a
Shikhar Kumar <shkumar@tripadvisor.com>
2020-05-04 00:10:39 -0400

    adding stubbed endpoints

---
+++ app.js
@@ -0,0 +1,47 @@
+// init setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// db connection
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = {
+  email: String,
+  password: String,
+  googleId: String,
+  secrets: [String]
+};
+
+const User = new mongoose.model("User", userSchema);
+
+// endpoints
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+// listener
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
